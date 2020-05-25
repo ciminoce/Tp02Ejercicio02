a0adca37cff936185c9228594294d6c2526abd8b
Carlos Enrique Cimino <ciminoce@yahoo.com.ar>
2020-05-25 11:36:30 -0300

    Agregar archivos de proyecto.

---
+++ Tp02Ejercicio02/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tp02Ejercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Signos del Zodíaco y Horóscopo Chino";
+            Console.ReadLine();
+        }
+    }
+}
